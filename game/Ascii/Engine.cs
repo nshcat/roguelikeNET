962f889a23da1741229f908f637d1da342077ad0
nshcat <nshcat@users.noreply.github.com>
2018-05-31 01:00:08 +0200

    initial commit

---
+++ game/Ascii/Engine.cs
@@ -0,0 +1,14 @@
+﻿namespace game.Ascii
+{
+    public static class Engine
+    {
+        /// <summary>
+        /// Initialize the engine global state and all sub systems.
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
+        public static void initialize(string[] args)
+        {
+            Native.EngineNative.initialize(args.Length, args);
+        }
+    }
+}
---
+++ game/Ascii/Native/EngineNative.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace game.Ascii.Native
+{
+    public static class EngineNative
+    {
+        /// <summary>
+        /// Initialize the engine global state and all sub systems.
+        /// </summary>
+        /// <param name="argc">Number of command line arguments</param>
+        /// <param name="argv">Command line arguments</param>
+        [DllImport("libascii.so")]
+        public static extern void initialize(Int64 argc, string[] argv);
+    }
+}
