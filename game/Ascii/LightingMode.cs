0810ea2f8d231a6ff91d8e86509d3c786a691c7d
nshcat <nshcat@users.noreply.github.com>
2018-07-26 20:50:42 +0200

    Added LightingMode enumeration

---
+++ game/Ascii/LightingMode.cs
@@ -0,0 +1,24 @@
+﻿namespace game.Ascii
+{
+    /// <summary>
+    /// An enumeration detailing how a particular cell interacts with the lighting system.
+    /// </summary>
+    public enum LightingMode
+    {
+        /// <summary>
+        /// Cell blocks light and does not receive any lighting at all.
+        /// </summary>
+        None = 0,
+        
+        /// <summary>
+        /// Cell blocks light, but receives a small amount of light. This is intended to
+        /// be used for walls of rooms.
+        /// </summary>
+        Dim  = 1,
+        
+        /// <summary>
+        /// Cell receives full lighting.
+        /// </summary>
+        Full = 2
+    }
+}
