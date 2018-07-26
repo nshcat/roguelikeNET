a69aa16ab0d373700ef5947716a7f2bfe452d686
nshcat <nshcat@users.noreply.github.com>
2018-07-26 20:31:25 +0200

    Added bindings to the lighting system of libascii

--- game/Ascii/Native/LightingNative.cs
+++ game/Ascii/Native/LightingNative.cs
@@ -1,4 +1,17 @@
-﻿$HEADER$namespace $NAMESPACE$
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace game.Ascii.Native
 {
-  public class $CLASS$ {$END$}
+    public static class LightingNative
+    {
+        [DllImport("libascii")]
+        public static extern UInt64 lighting_create_light(ref Light l);
+        
+        [DllImport("libascii")]
+        public static extern void lighting_destry_light(UInt64 handle);
+        
+        [DllImport("libascii")]
+        public static extern bool lighting_has_space(int count);
+    }
 }
