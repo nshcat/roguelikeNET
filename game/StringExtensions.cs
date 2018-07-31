f673f7c49dd0949ce3917ec1ad7eb81c970c0e54
nshcat <nshcat@users.noreply.github.com>
2018-07-31 23:09:43 +0200

    Moved example code to its own directory

--- game/StringExtensions.cs
+++ game/StringExtensions.cs
@@ -2,6 +2,12 @@
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// Pad string to given total length, applying padding on both sides.
+        /// </summary>
+        /// <param name="str">Input string</param>
+        /// <param name="length">Desired minimum toal length</param>
+        /// <returns>Padded string of minimal length <see cref="length"/></returns>
         public static string PadBoth(this string str, int length)
         {
             int spaces = length - str.Length;
