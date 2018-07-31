d4fe7ae03da25dcef6429b7701244bde39580305
nshcat <nshcat@users.noreply.github.com>
2018-07-31 02:00:37 +0200

    Implemented EntityFilter

---
+++ game/EntityComponent/EntityFilter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace game.EntityComponent
+{
+    /// <summary>
+    /// Base interface for all entity filter types. This exists in order to allow filter
+    /// that match different types of components to be stored in a single data structure without
+    /// the loss of type safety.
+    /// </summary>
+    public interface IEntityFilter
+    {
+        /// <summary>
+        /// Apply filter to given entity.
+        /// </summary>
+        /// <param name="e">Entity to apply filter to</param>
+        /// <returns>Flag indicating whether filter excludes or includes given entity</returns>
+        bool Apply(Entity e);
+    }
+
+    /// <summary>
+    /// A class representing a filter that can be used to filter sets of entities
+    /// based on whether they contain a component of a certain type and, optionally,
+    /// fulfil arbitrary conditions imposed on their current data.
+    /// </summary>
+    /// <typeparam name="T">The type of component to match for. Has to be derived from IComponent</typeparam>
+    public class EntityFilter<T> : IEntityFilter
+        where T : class, IComponent
+    {
+        /// <summary>
+        /// The predicate used to further filter entities that contain a component of
+        /// type T
+        /// </summary>
+        /// <remarks>
+        /// The default value is the trivial predicate that returns true for every possible
+        /// input.
+        /// </remarks>
+        protected Func<T, bool> Predicate
+        {
+            get;
+            set;
+        } = (_ => true);
+
+        /// <summary>
+        /// Create a filter that matches for existance of a component with type T.
+        /// Do not define any predicate to be applied to the data.
+        /// </summary>
+        public EntityFilter()
+        {
+            
+        }
+
+        /// <summary>
+        /// Create a filter that both matches for existance of a component with type T,
+        /// aswell as certain conditions imposed on the data managed by that component instance.
+        /// These conditions are implemented as a predicate.
+        /// </summary>
+        /// <param name="predicate">The predicate used to filter the entities further</param>
+        public EntityFilter(Func<T, bool> predicate)
+        {
+            Predicate = predicate;
+        }
+
+        /// <summary>
+        /// Apply filter to given entity.
+        /// </summary>
+        /// <param name="e">Entity to apply filter to</param>
+        /// <returns>Flag indicating whether filter excludes or includes given entity</returns>
+        public bool Apply(Entity e)
+        {
+            return e.HasComponent<T>() && Predicate(e.GetComponent<T>());
+        }
+    }
+}
