90417663229c75394c89e9feaac488b4b5795fa4
nshcat <nshcat@users.noreply.github.com>
2018-07-31 02:16:12 +0200

    Added some remark comments to IComponent.

--- game/EntityComponent/IComponent.cs
+++ game/EntityComponent/IComponent.cs
@@ -6,6 +6,12 @@ namespace game.EntityComponent
     /// <summary>
     /// The base interface for all components.
     /// </summary>
+    /// <remarks>
+    /// In addition to implementing all methods this interface defines, components
+    /// need to define a static property called "Identifier" which stores the unique
+    /// string identifier of that particular component type. This is important, since
+    /// they otherwise can't be referenced from JSON entity type definitions.
+    /// </remarks>
     public interface IComponent
     {
         /// <summary>
